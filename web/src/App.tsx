033cc1e170082d72560a3165dfd806cb5d1704e6
hanatobar <hanatobar27@gmail.com>
2026-08-25 17:27:19 +0300

    Hana/bank UI page (#9)
    * 5 databases done
    
    * bank ui page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,32 @@
 import { Routes, Route } from 'react-router-dom';
-import { Dashboard } from './pages/Dashboard';
 import { TransactionDetail } from './pages/TransactionDetail';
 import { Investigation } from './pages/Investigation';
 import { Feedback } from './pages/Feedback';
-import { FiOpsConsole } from './pages/FiOpsConsole';
 import { BusinessTreasury } from './pages/BusinessTreasury';
+import { BankLayout } from './components/BankLayout';
+import { BankDashboard, TransactionsPage, AlertsPage, AnalyticsPage, CustomersPage, MerchantsPage, PoliciesPage, ModelsPage, ReportsPage, AuditPage, SettingsPage } from './pages/BankPages';
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/tx/:id" element={<TransactionDetail />} />
-      <Route path="/investigation/:id" element={<Investigation />} />
-      <Route path="/feedback" element={<Feedback />} />
-      <Route path="/fi-ops" element={<FiOpsConsole />} />
-      <Route path="/treasury" element={<BusinessTreasury />} />
-    </Routes>
+    <BankLayout>
+      <Routes>
+        <Route path="/" element={<BankDashboard />} />
+        <Route path="/fi-ops" element={<BankDashboard />} />
+        <Route path="/bank/transactions" element={<TransactionsPage />} />
+        <Route path="/bank/alerts" element={<AlertsPage />} />
+        <Route path="/bank/analytics" element={<AnalyticsPage />} />
+        <Route path="/bank/customers" element={<CustomersPage />} />
+        <Route path="/bank/merchants" element={<MerchantsPage />} />
+        <Route path="/bank/policies" element={<PoliciesPage />} />
+        <Route path="/bank/models" element={<ModelsPage />} />
+        <Route path="/bank/reports" element={<ReportsPage />} />
+        <Route path="/bank/audit" element={<AuditPage />} />
+        <Route path="/bank/settings" element={<SettingsPage />} />
+        <Route path="/tx/:id" element={<TransactionDetail />} />
+        <Route path="/investigation/:id" element={<Investigation />} />
+        <Route path="/feedback" element={<Feedback />} />
+        <Route path="/treasury" element={<BusinessTreasury />} />
+      </Routes>
+    </BankLayout>
   );
 }
